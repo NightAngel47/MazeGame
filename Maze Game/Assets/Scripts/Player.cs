48530cc13c38bf8b6a0493487d7072e455908266
NightAngel47 <sjdrovie@gmail.com>
2015-06-26 15:05:40 -0500

    You Can Win!
    Tutorial has a victory thing now.

--- Maze Game/Assets/Scripts/Player.cs
+++ Maze Game/Assets/Scripts/Player.cs
@@ -1,30 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
 	public float speed = 4f;
+	public GameObject WinTrigger;
+	public Text winText;
+	public GameObject winTextObj;
 
+	private int lvlnum = 1;
+	private bool checkWin = false;
 	private Rigidbody2D rb2D;
 
 	// Use this for initialization
 	void Awake () {
 		rb2D = GetComponent<Rigidbody2D>();
+		winTextObj.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey(KeyCode.D)) {
-			rb2D.AddForce(Vector2.right * speed);
+
+		playerMovement(checkWin);
+
+		if( checkWin == true) {
+			winText.text = "You Completed Level " + lvlnum + "!!!" + "\n" + "Congrats!!!";
+			winTextObj.SetActive(true);
 		}
-		if(Input.GetKey(KeyCode.A)) {
-			rb2D.AddForce(Vector2.left * speed);
+	}
+
+	//OnTriggerEnter2D is sent when another object enters a trigger collider attached to this object (2D physics only).
+	private void OnTriggerEnter2D (Collider2D other) {
+		//Check if the tag of the trigger collided with is Exit.
+		if(other.tag == "Win") {
+			checkWin = true;
 		}
-		if(Input.GetKey(KeyCode.W)) {
-			rb2D.AddForce(Vector2.up * speed);
-		}
-		if(Input.GetKey(KeyCode.S)) {
-			rb2D.AddForce(Vector2.down * speed);
+	}
+
+	private void playerMovement(bool win) {
+		if(win == false) {
+			if(Input.GetKey(KeyCode.D)) {
+				rb2D.AddForce(Vector2.right * speed);
+			}
+			if(Input.GetKey(KeyCode.A)) {
+				rb2D.AddForce(Vector2.left * speed);
+			}
+			if(Input.GetKey(KeyCode.W)) {
+				rb2D.AddForce(Vector2.up * speed);
+			}
+			if(Input.GetKey(KeyCode.S)) {
+				rb2D.AddForce(Vector2.down * speed);
+			}
 		}
 	}
 }
