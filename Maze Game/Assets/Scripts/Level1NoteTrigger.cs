95f6421b788f690351a38d424989cb3a98b01151
NightAngel47 <sjdrovie@gmail.com>
2015-08-11 19:27:40 -0500

    Now with a Story
    Ok so I've got the notes working and displaying the story. Right now it
    doesn't look amazing, but it's there also I'm probably going to do some
    optimization by destroying and loading levels as needed. I thought maybe
    we could design levels in the levels scene and have a different scene
    where the game plays the levels. (I do know how to do this I would
    rather show you what I'm talking about it person so i can make more
    sense. Oh and no progress so far on the moving level. That I can try.
    And there is no new build yet the next one would probably be when the
    optimization is done.

---
+++ Maze Game/Assets/Scripts/Level1NoteTrigger.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level1NoteTrigger : MonoBehaviour {
+	
+	[HideInInspector]
+	public bool isReadingNote = false;
+	public GameObject level1Note;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		doneLevel1Note();
+	}
+
+	//OnTriggerEnter2D is sent when another object enters a trigger collider attached to this object (2D physics only).
+	private void OnTriggerEnter2D (Collider2D other) {
+		//if the player finds the note on level 1 then show it
+		if(other.tag == "Level1Note") {
+			showLevel1Note();
+		}
+	}
+
+	//show the note for level 1
+	private void showLevel1Note() {
+		level1Note.SetActive(true);
+		isReadingNote = true;
+		
+	}
+	//hide the note for level 1
+	private void doneLevel1Note() {
+		if(isReadingNote == true && Input.GetKeyDown("space")) {
+			level1Note.SetActive(false);
+			isReadingNote = false;
+		}
+	}
+}
--- Maze Game/Assets/Scripts/PlayerMovement.cs
+++ Maze Game/Assets/Scripts/PlayerMovement.cs
@@ -5,27 +5,30 @@ public class PlayerMovement : MonoBehaviour {
 	
 	public float speed = 8f;
 
+	private bool isreading = false;
 	private Rigidbody2D rb2D;
 	private StartingText startText;
 	private ExitTriggers exitTriggers;
-	private StoryNotes storyNotes;
+	private Level1NoteTrigger L1NT;
+	private Level3NoteTrigger L3NT;
 	
 	// Use this for initialization
 	void Awake() {
 		rb2D = GetComponent<Rigidbody2D>();
 		startText = GetComponent<StartingText>();
 		exitTriggers = GetComponent<ExitTriggers>();
-		storyNotes = GetComponent<StoryNotes>();
+		L1NT = GetComponent<Level1NoteTrigger>();
+		L3NT = GetComponent<Level3NoteTrigger>();
 	}
 
 	// Update is called once per frame
 	void Update() {
-		
+		reading(L1NT.isReadingNote, L3NT.isReadingNote);
 	}
 
 	void FixedUpdate() {
 		//moves player while game is active and player is not doing something else
-		playerMovement(exitTriggers.checkWin, exitTriggers.checkAltExit, startText.checkStart, storyNotes.isReadingNote);
+		playerMovement(exitTriggers.checkWin, exitTriggers.checkAltExit, startText.checkStart, isreading);
 	}
 
 	//moves player if they aren't doing something else
@@ -44,6 +47,17 @@ public class PlayerMovement : MonoBehaviour {
 			if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
 				rb2D.AddForce(Vector2.down * speed);
 			}
+		}
+	}
+
+	private void reading(bool l1NT, bool l3NT) {
+
+		if(l1NT == true || l3NT == true) {
+			isreading = true;
+		}
+
+		if(l1NT == false || l3NT == false) {
+			isreading = false;
 		}
 	}
 }
