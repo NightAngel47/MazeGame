8e349c55f7279abdef75237121017db077f394b4
NightAngel47 <sjdrovie@gmail.com>
2015-08-12 20:30:00 -0500

    Done for now
    You can delete the testing level

---
+++ Maze Game/Assets/Scripts/Level4MovingTiles.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level4MovingTiles : MonoBehaviour {
+
+	public float moveSpeed = 0.3f;
+	public float delayTime;
+	
+	public GameObject tile1;
+	public GameObject tile2;
+	public GameObject tile3;
+	public GameObject tile4;
+	public GameObject tile5;
+	public GameObject tile6;
+	public GameObject tile7;
+	public GameObject tile8;
+	public GameObject emptySpace;
+	public GameObject movingBarriers;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
